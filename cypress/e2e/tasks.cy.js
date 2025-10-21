f99f855ad68d7ddf80c4c2ee33e8de7cfdd4f0e8
Anastasia Gegel <anastasiagegel04@mail.ru>
2025-10-22 01:50:11 +0800

    fix: add env file to cypress

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,19 +1,22 @@
 describe("Task Manager E2E", () => {
+  const backendUrl = Cypress.env("BACKEND_URL") || "http://localhost:5001";
+  const frontendUrl = Cypress.env("FRONTEND_URL") || "http://localhost:3000";
+
   beforeEach(() => {
     cy.request({
       method: "POST",
-      url: "http://localhost:5001/test/clear",
-      failOnStatusCode: false, 
+      url: `${backendUrl}/test/clear`,
+      failOnStatusCode: false,
     });
   });
 
   it("Показывает пустой список задач, если база пуста", () => {
-    cy.visit("http://localhost:3000"); 
+    cy.visit(frontendUrl);
     cy.get("[data-testid='task-item']").should("have.length", 0);
   });
 
   it("Можно добавить новую задачу и она отображается", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit(frontendUrl);
 
     cy.get("[data-testid='task-input']").type("Новая задача");
     cy.get("[data-testid='task-add-button']").click();
@@ -25,14 +28,14 @@ describe("Task Manager E2E", () => {
   });
 
   it("Задачу можно отметить выполненной", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit(frontendUrl);
 
     cy.get("[data-testid='task-input']").type("Новая задача");
     cy.get("[data-testid='task-add-button']").click();
 
     cy.get("[data-testid='task-item']")
       .contains("Новая задача")
-      .click(); 
+      .click();
 
     cy.get("[data-testid='task-item']")
       .contains("Новая задача")
