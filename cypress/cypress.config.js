baf897f0fec55ca32d5b6a353fe8f6c16a86d869
Anastasia Gegel <anastasiagegel04@mail.ru>
2025-11-04 21:35:40 +0800

    fix: delete baseUrl in cypress config file

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: process.env.CYPRESS_baseUrl || "http://frontend:80",
     supportFile: false,
     specPattern: "e2e/**/*.cy.{js,jsx,ts,tsx}"
   },
